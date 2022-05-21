0ebe87a2ba523026188c1a114914396f267e6b51
JSNielsen <cph1968@protonmail.com>
2022-05-21 07:58:12 +0200

    summary upload
    upload all files

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to buckingham
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/buckingham/core_test.clj
@@ -0,0 +1,7 @@
+(ns buckingham.core-test
+  (:require [clojure.test :refer :all]
+            [buckingham.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
